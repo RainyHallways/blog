c7fe6f47cf2ced56445c7cdece1f3fd9cc356fe9
RainyHallways <78735995+RainyHallways@users.noreply.github.com>
2025-09-13 22:31:30 +0800

    init

---
+++ apps/web/app/layout.config.tsx
@@ -0,0 +1,66 @@
+import { Icons } from "@/components/icons";
+import type { BaseLayoutProps, LinkItemType } from "fumadocs-ui/layouts/shared";
+import Image from "next/image";
+
+export const title = "rjv.im";
+export const description =
+  "Personal website of rjv.im. This is where I articulate my work, open source projects, thoughts, ideas, work, commentary and opinions.";
+export const owner = "rjv.im";
+/**
+ * Shared layout configurations
+ *
+ * you can customise layouts individually from:
+ * Home Layout: app/(home)/layout.tsx
+ * Docs Layout: app/docs/layout.tsx
+ */
+export const baseOptions: BaseLayoutProps = {
+  nav: {
+    title: (
+      <>
+        <Image
+          src="/avatar.png"
+          width={28}
+          height={28}
+          alt="RainyHallways avatar"
+          className="rounded-full"
+        />{" "}
+        RainyHallways
+      </>
+    ),
+  },
+  links: [
+    {
+      text: "Docs",
+      url: "/docs",
+    },
+  ],
+};
+
+export const linkItems: LinkItemType[] = [
+  {
+    icon: <Icons.info />,
+    text: "Blog",
+    url: "/blog",
+    active: "url",
+  },
+  {
+    icon: <Icons.info />,
+    text: "About",
+    url: "/about",
+    active: "url",
+  },
+  // {
+  //   icon: <Icons.posts />,
+  //   text: "Me",
+  //   url: "/me",
+  //   active: "url",
+  // },
+  // {
+  //   icon: <Icons.tags />,
+  //   text: "Tags",
+  //   url: "/tags",
+  //   active: "url",
+  // },
+];
+
+export const postsPerPage = 5;
